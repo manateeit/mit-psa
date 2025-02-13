4b6760178ef94f9913487e13340538804594bd39
Robert Isaacs <robert@nineminds.com>
2025-02-13 10:02:54 -0500

    fix: citus table compatibility

--- server/migrations/20250212094500_remove_ticket_number_trigger.cjs
+++ server/migrations/20250212094500_remove_ticket_number_trigger.cjs
@@ -11,7 +11,7 @@ exports.up = async function(knex) {
         SELECT EXISTS (
           SELECT 1
           FROM citus_tables
-          WHERE table_name = 'tickets'
+          WHERE citus_tables.table_name::text = 'tickets'
         )
       `);
 
