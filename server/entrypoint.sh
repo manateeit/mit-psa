73945d7bee06ca66b0d7ca94f9feea89a1f5bbea
Robert Isaacs <robert@nineminds.com>
2025-03-05 10:50:00 -0500

    chore: bump version number

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -26,9 +26,9 @@ get_secret() {
 
 # Function to print version banner
 print_version_banner() {
-    version="0.0.16"
+    version="0.0.17"
     commit="abc123"
-    date="25-02-2025"
+    date="05-03-2025"
     author="NineMinds"
 
     # Function to print colored text
