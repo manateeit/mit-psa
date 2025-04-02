707ddb3f83f1671738acbee583e10e09d61d1287
Robert Isaacs <robert@nineminds.com>
2025-04-02 14:48:29 -0400

    build: repair docker build for pgbouncer

--- pgbouncer/entrypoint.sh
+++ pgbouncer/entrypoint.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 set -e
 
-# Create temporary file for userlist
-TEMP_USERLIST=$(mktemp)
+# Read secrets
+POSTGRES_PASSWORD=$(cat /run/secrets/postgres_password)
+DB_PASSWORD_SERVER=$(cat /run/secrets/db_password_server)
 
-# Generate userlist.txt with passwords from secrets
-echo "\"postgres\" \"$(cat /run/secrets/postgres_password)\"" > $TEMP_USERLIST
-echo "\"app_user\" \"$(cat /run/secrets/db_password_server)\"" >> $TEMP_USERLIST
+# Escape potential special characters in passwords for sed
+POSTGRES_PASSWORD_ESC=$(echo "$POSTGRES_PASSWORD" | sed -e 's/[\/&]/\\&/g')
+DB_PASSWORD_SERVER_ESC=$(echo "$DB_PASSWORD_SERVER" | sed -e 's/[\/&]/\\&/g')
 
-# Copy to pgbouncer directory with correct permissions
-cat $TEMP_USERLIST > /etc/pgbouncer/userlist.txt
-rm $TEMP_USERLIST
+# Substitute placeholders in userlist.txt with actual passwords from secrets
+# Using '#' as delimiter for sed to avoid issues with '/' in passwords
+sed -i "s#POSTGRES_PASSWORD_PLACEHOLDER#$POSTGRES_PASSWORD_ESC#g" /etc/pgbouncer/userlist.txt
+sed -i "s#DB_PASSWORD_SERVER_PLACEHOLDER#$DB_PASSWORD_SERVER_ESC#g" /etc/pgbouncer/userlist.txt
 
-# Ensure proper permissions
+# Ensure proper ownership if running as root (permissions set during build)
 if [ "$(id -u)" = "0" ]; then
-  chmod 600 /etc/pgbouncer/userlist.txt
   chown pgbouncer:pgbouncer /etc/pgbouncer/userlist.txt
 fi
 
